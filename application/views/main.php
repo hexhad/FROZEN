be239459be16894c09c107f3128adda196a461ee
Hashan <48557376+hexhad@users.noreply.github.com>
2020-02-06 13:37:48 +0530

    slider1

--- application/views/main.php
+++ application/views/main.php
@@ -44,17 +44,16 @@
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum praesentium in nobis optio labore repellendus dicta voluptatum corrupti aliquid itaque magni animi, temporibus a consequuntur doloribus delectus sunt! Voluptate, ratione!</p>
         </div>
         <div class="col-md-6">
-                    <!-- <img class="img-fluid" src="src/frozen.jpg"> -->
           <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img class="d-block w-100" src="src/frozen.jpg" alt="First slide">
+                <img class="d-block w-100" src="src/latest_product/slide1.png" alt="First slide">
               </div>
               <div class="carousel-item">
-                <img class="d-block w-100" src="src/frozen.jpg" alt="Second slide">
+                <img class="d-block w-100" src="src/latest_product/slide1.png" alt="Second slide">
               </div>
               <div class="carousel-item">
-                <img class="d-block w-100" src="src/frozen.jpg" alt="Third slide">
+                <img class="d-block w-100" src="src/latest_product/slide1.png" alt="Third slide">
               </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
