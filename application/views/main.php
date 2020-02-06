672dd4fcbc2acd4c36b1286227698d88fa2d5105
Hashan <48557376+hexhad@users.noreply.github.com>
2020-02-06 13:09:55 +0530

    Update main.php
    SliderUpdate_1

--- application/views/main.php
+++ application/views/main.php
@@ -44,7 +44,28 @@
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum praesentium in nobis optio labore repellendus dicta voluptatum corrupti aliquid itaque magni animi, temporibus a consequuntur doloribus delectus sunt! Voluptate, ratione!</p>
             </div>
             <div class="col-md-6">
-                <img class="img-fluid" src="src/frozen.jpg">
+                        <!-- <img class="img-fluid" src="src/frozen.jpg"> -->
+              <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="src/frozen.jpg" alt="First slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="src/frozen.jpg" alt="Second slide">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="src/frozen.jpg" alt="Third slide">
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
             </div>
         </div>
     </div>
