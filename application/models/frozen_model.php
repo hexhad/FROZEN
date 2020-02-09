67212a1a42b9e76f53fd23c7e03942c177f33306
Hashan <hexhad@users.noreply.github.com>
2020-02-10 00:34:10 +0530

    hashan
    tabs

--- application/models/frozen_model.php
+++ application/models/frozen_model.php
@@ -8,5 +8,10 @@ class frozen_model extends CI_Model {
         return $this->db->get('product')->result();
 	}
 
+	public function view_a_record($id)
+	{
+		return $this->db->get_where('product', ['Product_id' => $id ])->result();
+	}
+
 	
 }
